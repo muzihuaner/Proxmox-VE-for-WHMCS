61ce24ab36110207ed5c18dea649006d0ed8894f
Luke S Thompson <73861168+lsthompson@users.noreply.github.com>
2024-06-11 10:17:55 +1000

    Resolve syntax issues (fix #77)

--- modules/addons/pvewhmcs/db.sql
+++ modules/addons/pvewhmcs/db.sql
@@ -2,13 +2,13 @@ CREATE TABLE IF NOT EXISTS `mod_pvewhmcs` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `config` varchar(255),
   `vnc_secret` varchar(255),
-  `debug_mode` tinyint(1) unsigned DEFAULT '0',
+  `debug_mode` tinyint(1) unsigned DEFAULT 0,
   PRIMARY KEY (`id`)
 );
 INSERT INTO `mod_pvewhmcs` (`id`, `config`, `vnc_secret`) VALUES	(1, NULL, NULL);
 CREATE TABLE IF NOT EXISTS `mod_pvewhmcs_ip_addresses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pool_id` int(11) NOT NULL DEFAULT '0',
+  `pool_id` int(11) NOT NULL DEFAULT 0,
   `ipaddress` varchar(255) NOT NULL DEFAULT '0',
   `mask` varchar(255) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
@@ -40,15 +40,15 @@ CREATE TABLE IF NOT EXISTS `mod_pvewhmcs_plans` (
   `diskio` varchar(20) DEFAULT '0',
   `netmode` varchar(10) DEFAULT NULL,
   `bridge` varchar(20) NOT NULL DEFAULT 'vmbr',
-  `vmbr` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `vmbr` tinyint(1) unsigned NOT NULL DEFAULT 0,
   `netmodel` varchar(10) DEFAULT NULL,
   `netrate` varchar(5) DEFAULT '0',
-  `firewall` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `bw` int(6) unsigned DEFAULT '0',
-  `kvm` tinyint(1) unsigned DEFAULT '0',
-  `onboot` tinyint(1) unsigned DEFAULT '0',
+  `firewall` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `bw` int(6) unsigned DEFAULT 0,
+  `kvm` tinyint(1) unsigned DEFAULT 0,
+  `onboot` tinyint(1) unsigned DEFAULT 0,
   `vlanid` varchar(10) DEFAULT NULL,
-  `ipv6` varchar(10) unsigned DEFAULT 'auto',
+  `ipv6` varchar(10) DEFAULT 'auto',
   PRIMARY KEY (`id`)
 );
 CREATE TABLE IF NOT EXISTS `mod_pvewhmcs_vms` (
